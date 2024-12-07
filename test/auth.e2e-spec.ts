704aff3d8ce4ad3745945280b5a626ce7b8cbd36
duyviet2101 <whoisduyviet@gmail.com>
2024-12-07 17:38:49 +0700

    env vars and end to end test (2)

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -26,4 +26,22 @@ describe('Authentication System', () => {
     expect(id).toBeDefined();
     expect(email).toEqual(emailInp);
   });
+
+  it('signup as new user thenn get the currently logged in user', async () => {
+    const email = 'asdff@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
---
+++ test/setup.ts
@@ -0,0 +1,9 @@
+import { rm } from 'fs/promises';
+import { join } from 'path';
+
+global.beforeEach(async () => {
+  try {
+    await rm(join(__dirname, '..', 'test.sqlite'));
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {}
+});
